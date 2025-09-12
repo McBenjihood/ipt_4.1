563392feef57b34df0ea55b24b324b4c8e89b868
McBenjihood <benjaminmikagerresheim@gmail.com>
2025-09-12 08:25:00 +0200

    Testing workflow stuff...

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
-
 export default defineConfig({
+  base: '/ipt_4.1/',
   plugins: [
     vue(),
   ],
